8f32d11f2fc7264b171299c604cdc0d26874a507
CJ <treasure32109@gmail.com>
2026-05-02 22:03:53 +0100

    Incominggggg ✈️

--- Week_7/LegacyOrderFacade.java
+++ Week_7/LegacyOrderFacade.java
@@ -1,5 +1,14 @@
 package Week_7;
 
 public class LegacyOrderFacade {
-    
+
+    private final LegacyOrderProcessor legacyAgent;
+
+    public LegacyOrderFacade() {
+        this.legacyAgent = new LegacyOrderProcessor();
+    }
+
+    public void executeOrder(String email, String id, double cost, String destination) {
+        legacyAgent.processOrder(email, id, cost, destination);
+    }
 }
